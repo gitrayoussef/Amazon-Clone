70093f8bf833885604790eebae4bf599913f705b
ranayoussef <ryoussefabuelkomsan@gmail.com>
2023-02-13 13:36:30 +0200

    Frontend & Backend setup for the project + Intial Models and Migration

---
+++ Backend/database/migrations/2023_02_13_070852_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('desc');
+            $table->foreignId('category_id')->references('id')->on('product_category');
+            $table->foreignId('inventory_id')->references('id')->on('product_inventory');
+            $table->decimal('price');
+            $table->foreignId('discount_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
